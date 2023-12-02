63518526467fba3537b85eb1d3b1228c6fd2a707
YourAva <127442971+YourAva@users.noreply.github.com>
2023-12-02 21:26:14 +0000

    Custom Update YML
    General update to make the Custom

--- Custom Join Message/src/main/java/com/mellurboo/customjoinmessage/CustomJoinMessage.java
+++ Custom Join Message/src/main/java/com/mellurboo/customjoinmessage/CustomJoinMessage.java
@@ -1,9 +1,11 @@
 package com.mellurboo.customjoinmessage;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -11,61 +13,31 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class CustomJoinMessage extends JavaPlugin implements Listener {
 
     public String prefix = "[CustomJoinMessage] ";
-    public String[] playerJoinMessages = {
-            "§ewhat a shame",
-            "§cbe careful, the new one bites,",
-            "§cLook out for the one from birmingham,",
-            "§ebring class A drugs only!!!!",
-            "§eLook out,",
-            "§dOnly you can prevent v-bucks scams,",
-            "§aTHATS WHAT THE MASK ISSSSDDAS,",
-            "§afaking speedruns is not allowed!",
-            "§eAll thigh pics must go through izzy first,",
-            "§ayou want to play lets play,",
-            "§dWindows fanboy",
-            "§eTopG",
-            "§cWatch your wallets!",
-            "§dSilver instalock reyna main",
-            "§cyou feel a chill go down your spine,",
-            "gravity feels 3x stronger"
+    public String[] playerJoinMessages;
+    public String[] playerQuitMessages;
 
-    };
-
-    public String[] playerQuitMessages = {
-            "§eFinally",
-            "§dWhat a pussy",
-            "§eCombat log?",
-            "§dfreelo",
-            "§2this is a nice color. anyway",
-            "§3Peace control",
-            "§eGGEZ",
-            "§3that one person who thinks theyre part of the friend group",
-            "§aSkibbidi toilet 69420",
-            "§e(Willy watcher 9000)",
-            "§eI think",
-            "§eAnd keep the change you filthy animal"
-    };
-
-    public String[] playerDeathMessages = {
-            "§fLMFAO",
-            "§cGGEZ",
-            "§fUtterly Shameful",
-            "§eKeep inventory is not enabled buddy",
-            "§cFF",
-            "§fOpen your eyes kid",
-            "§fin what world does",
-            "§fthats peak...",
-            "§c'is it hardcore?'",
-            "§fkaput,",
-            "§elag ->"
-
-    };
+//    public String[] playerDeathMessages = {
+//            "§fLMFAO",
+//            "§cGGEZ",
+//            "§fUtterly Shameful",
+//            "§eKeep inventory is not enabled buddy",
+//            "§cFF",
+//            "§fOpen your eyes kid",
+//            "§fin what world does",
+//            "§fthats peak...",
+//            "§c'is it hardcore?'",
+//            "§fkaput,",
+//            "§elag ->"
+//
+//    };
 
 
     @Override
     public void onEnable() {
+        loadConfig();
         getServer().getPluginManager().registerEvents(this, this);
         getLogger().info(prefix + "Ready");
+        getCommand("reloadCustomJoinMessage").setExecutor(this);
     }
 
     @Override
@@ -73,23 +45,41 @@ public final class CustomJoinMessage extends JavaPlugin implements Listener {
         Bukkit.getConsoleSender().sendMessage(prefix + "Shutdown Initiated");
     }
 
+    private void loadConfig() {
+        saveDefaultConfig();
+        FileConfiguration config = getConfig();
+
+        //Load Join Messages from config.yml
+        playerJoinMessages = config.getStringList("playerJoinMessages").stream()
+                .toArray(String[]::new);
+        playerQuitMessages = config.getStringList("playerQuitMessages").stream()
+                .toArray(String[]::new);
+    }
+
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        event.setJoinMessage(null);
-
-        String playerName = event.getPlayer().getName();
-
-        int randomSentence = (int) (Math.random() * playerJoinMessages.length - 1);
-        Bukkit.broadcastMessage(playerJoinMessages[randomSentence] + " " + playerName + " has joined the Server");
+        event.setJoinMessage(playerJoinMessages[(int) (Math.random() * playerJoinMessages.length - 1)] + " " + event.getPlayer().getName() + " has joined the Server");
     }
 
     @EventHandler
     public void onPlayerLeave(PlayerQuitEvent event){
-        event.setQuitMessage(null);
-        String playerName = event.getPlayer().getName();
+        event.setQuitMessage(playerQuitMessages[(int) (Math.random() * playerQuitMessages.length - 1)] + " " + event.getPlayer().getName() + " has left the Server");
+    }
 
-        int randomSentence = (int) (Math.random() * playerQuitMessages.length - 1);
-        Bukkit.broadcastMessage(playerQuitMessages[randomSentence] + " " + playerName + " has left the Server");
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (label.equalsIgnoreCase("reloadCustomJoinMessage")) {
+            if (sender.hasPermission("CustomJoinMessage.reloadconfig")) {
+                reloadConfig();
+                loadConfig(); // Reload your custom configuration loading logic
+                sender.sendMessage("§aCustom Join Message config reloaded.");
+                return true;
+            } else {
+                sender.sendMessage("§cYou don't have permission to use this command.");
+                return true;
+            }
+        }
+        return false;
     }
 /*
     @EventHandler
